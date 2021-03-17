b2e8e61bbc0d5351e842475ffd4be549c991afa9
Elamathi <Elamathi@Lenovo-PC>
2021-03-17 13:31:44 +0530

    Updated explicit wait class

--- src/testScripts/TC15_ExplicitWait.java
+++ src/testScripts/TC15_ExplicitWait.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import sun.tools.jconsole.Plotter.Unit;
-
 public class TC15_ExplicitWait {
 
 	public static void main(String[] args) {
